64ff07c4c43aaa0672a1f450121a32eb8d48791a
fdhhhdjd <nguyentientai10@gmail.com>
2024-06-20 11:40:29 +0700

    feat: add donate github

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import Demo from '@/components/Demo';
-
 const App = () => {
   const [count, __] = React.useState(0);
 
@@ -9,7 +7,6 @@ const App = () => {
     <React.Fragment>
       <p className="text-3xl font-bold underline">Hello</p>
       <h2>{count}</h2>
-      <Demo />
     </React.Fragment>
   );
 };
--- src/components/Demo.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Demo = () => <React.Fragment>Demo</React.Fragment>;
-
-export default Demo;
